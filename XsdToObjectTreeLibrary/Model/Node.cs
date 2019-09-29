6acb1ad2345c70b1f46546f7a0c3ceb6eb689b27
Daniel Saunders <daniel@exatetechnology.com>
2019-09-29 09:03:07 +0100

    add default initialisers for collections to prevent null reference errors

--- XsdToObjectTreeLibrary/Model/Node.cs
+++ XsdToObjectTreeLibrary/Model/Node.cs
@@ -9,8 +9,8 @@ namespace XsdToObjectTreeLibrary.Model
         public string Name { get; set; }
         public string DisplayName { get; set; }
         public string NodePath { get; set; }
-        public List<Node> Children { get; set; }
-        public List<string> Attributes { get; set; }
+        public List<Node> Children { get; set; } = new List<Node>();
+        public List<string> Attributes { get; set; } = new List<string>();
         public NodeTypeEnum NodeType { get; set; }
     }
 }
