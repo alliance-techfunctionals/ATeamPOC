e9d896cc4029a683372a798c9c803c4389f6816b
Sneyden <sneyden@gmail.com>
2019-10-04 12:00:06 +0530

    Added json test case for full file test and only elements with children test

--- XUnitTestProject/XsdToTreeTest.cs
+++ XUnitTestProject/XsdToTreeTest.cs
@@ -2,6 +2,7 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Schema;
 using XsdToObjectTree.UnitTest.Helper;
@@ -27,8 +28,21 @@ namespace XUnitTestProject
             var target = new XsdToTree();
             var result = target.AnalyseSchema(xss);
 
+            // contains all elements in the xsd file
             var jsonStr = JsonConvert.SerializeObject(result);
+
+            // contains only elements with children to try and match the json output provided
             var jsonOnlyChildren = JsonConvert.SerializeObject(result.Children.Where(n => n.Children != null && n.Children.Count > 0));
+
+            var expectedJsonStr = File.ReadAllText("sampleoutputs/demo3_updated.example.json");
+            var expectedJsonChildrenStr = File.ReadAllText("sampleoutputs/demo3_updated_children.example.json");
+            //Assert.Equal()
+
+            var cleanJsonStr = Regex.Replace(expectedJsonStr, @"\s+", "");
+            var cleanJsonChildrenStr = Regex.Replace(expectedJsonChildrenStr, @"\s+", "");
+
+            Assert.Equal(cleanJsonStr, jsonStr);
+            Assert.Equal(cleanJsonChildrenStr, jsonOnlyChildren);
         }
     }
 }
