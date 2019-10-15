147c9f9599acb3b68ad0079e9306ee7b2a23e9a8
Neha ateam <nvnehaverma40@gmail.com>
2019-10-15 17:38:29 +0530

    Batch Test file for test the Test Cases.

---
+++ XUnitTestProject/Batch/BatchTester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XsdToObjectTreeLibrary.Batch;
+using Xunit;
+
+namespace XUnitTestProject.Batch
+{
+    public class BatchTester
+   {
+        [Fact]
+        public void BatchMethod()
+        {
+
+            long newFileSize = XmlBatchHelper.GetBatchMaxElementCount(5000, 521);
+        }
+   }
+}
