ac27af237f8fc75671fb54bae135cf3f2debfdab
Neha ateam <nvnehaverma40@gmail.com>
2019-10-23 17:38:57 +0530

    changed the code to avoid hard code column name

--- Oracle/Ex8.SqlDml/Ex8.SqlDml/Writer/Dbms/OracleSqlWriter.cs
+++ Oracle/Ex8.SqlDml/Ex8.SqlDml/Writer/Dbms/OracleSqlWriter.cs
@@ -1,10 +1,12 @@
 ﻿using Dapper;
+using Ex8.EtlModel;
 using Ex8.EtlModel.DatabaseJobManifest;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text;
 
 namespace Ex8.SqlDml.Writer.Dbms
 {
@@ -28,31 +30,88 @@ namespace Ex8.SqlDml.Writer.Dbms
             using (var connection = new OracleConnection(connectionString))
             {
                 connection.Open();
-                int[] ids = new int[data.Rows.Count];
-                string[] fnames = new string[data.Rows.Count];
-                string[] lnames = new string[data.Rows.Count];
+                int[] ids = new int[data.Rows.Count];              
 
                 for (int j = 0; j < data.Rows.Count; j++)
                 {
-                    ids[j] =    Convert.ToInt32(data.Rows[j]["PERSON_ID"]);
-                    fnames[j] = Convert.ToString(data.Rows[j]["FIRST_NAME"]);
-                    lnames[j] = Convert.ToString(data.Rows[j]["LAST_NAME"]);
+                    ids[j] = Convert.ToInt32(data.Rows[j][tableInfo.pk_column_name]);                    
                 }
+             
+                StringBuilder sql = new StringBuilder("INSERT INTO " + destinationTableName + " (" + tableInfo.pk_column_name);
+                StringBuilder values = new StringBuilder("VALUES ( :"+ tableInfo.pk_column_name);
 
-                var id = new OracleParameter { OracleDbType = OracleDbType.Int32, Value = ids };
-                var name = new OracleParameter { OracleDbType = OracleDbType.Varchar2, Value = fnames };
-                var lname = new OracleParameter { OracleDbType = OracleDbType.Varchar2, Value = lnames };
-
+                foreach (var col in tableInfo.columns)
+                {
+                    sql.Append(","+col.columnName);  
+                    values.Append(",:" + col.columnName);
+                }
+                sql.Append(") ");
+                sql.Append(values.ToString());
+                sql.Append(")");
+              
                 // create command and set properties  
-                OracleCommand cmd = connection.CreateCommand();                                
-                cmd.CommandText = $"INSERT INTO {destinationTableName} (PERSON_ID, FIRST_NAME, LAST_NAME) VALUES (:1, :2, :3)";                            
+                OracleCommand cmd = connection.CreateCommand();              
+                cmd.CommandText = sql.ToString();
                 cmd.ArrayBindCount = ids.Length;
-                cmd.Parameters.Add(id);
-                cmd.Parameters.Add(name);
-                cmd.Parameters.Add(lname);
+
+                //get primary column name and its data type                
+                var pkColumnName = tableInfo.pk_column_name;
+                var pkColumntype = tableInfo.pk_data_type;
+
+                cmd.Parameters.Add(new OracleParameter { OracleDbType = OracleDbType.Int32, Value = ids });
+                // for datatable columns
+                foreach (var tabinfocol in tableInfo.columns)
+                {
+                    // get other columns name and its data type exclude primary key column
+                    var columnName = tabinfocol.columnName;
+                    var columntype = tabinfocol.dataType;
+                    // for datatable rows
+                    OracleParameter p = new OracleParameter();                                        
+                    List<dynamic> colValueArr = new List<dynamic>(data.Rows.Count);
+                    foreach (DataRow dr in data.Rows)
+                    {
+                        var colValue = dr[columnName].ToString();                       
+                       // case type switch
+                        switch (columntype.ToLower())
+                        {
+                            case "number":
+                                p.OracleDbType = OracleDbType.Decimal;
+                                colValueArr.Add(Convert.ToInt32(colValue));
+                                break;
+                            case "varchar2":
+                                p.DbType = DbType.String;
+                                colValueArr.Add(Convert.ToString(colValue));
+                                break;                           
+                            case "datetime":
+                            case "date":
+                                p.DbType = DbType.DateTime;
+                                colValueArr.Add(Convert.ToDateTime(colValue));
+                                break;                            
+                            case "text":
+                                p.DbType = DbType.String;
+                                colValueArr.Add(Convert.ToString(colValue));
+                                break;
+                            default:
+                                break;
+                        }                                             
+                    }
+                    p.Value = colValueArr.ToArray();                   
+                    cmd.Parameters.Add(p);
+                }
 
                 return cmd.ExecuteNonQuery();
             }
-        } 
+
+        }
+        //public int UpdateBulkData(string connectionString,string UpdateFromTempDml)
+        //{
+        //    using (var connection = new OracleConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        OracleCommand cmd = connection.CreateCommand();
+        //        cmd.CommandText = UpdateFromTempDml;
+        //        return cmd.ExecuteNonQuery();
+        //    }               
+        //}
     }
 }
