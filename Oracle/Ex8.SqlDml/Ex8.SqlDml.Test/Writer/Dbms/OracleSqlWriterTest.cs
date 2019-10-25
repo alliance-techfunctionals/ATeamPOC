1474543c14958a790ccc85664e35d54a3571cf7b
Neha ateam <nvnehaverma40@gmail.com>
2019-10-25 23:08:29 +0530

    new test case for Update

--- Oracle/Ex8.SqlDml/Ex8.SqlDml.Test/Writer/Dbms/OracleSqlWriterTest.cs
+++ Oracle/Ex8.SqlDml/Ex8.SqlDml.Test/Writer/Dbms/OracleSqlWriterTest.cs
@@ -16,38 +16,40 @@ namespace Ex8.SqlDml.Test.Writer
     public class OracleSqlWriterTest
     {
         private const string _inputRoot = "TestData\\Input\\";
-        //private const string connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=tcp)(HOST=oracle1.sql.exatebot.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=xepdb1)));User Id=TEST_USER;Password=ExateDbUser123!;";
+        private const string connectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=oracle1.sql.exatebot.com)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=xepdb1)));User Id=TEST_USER;Password=ExateDbUser123!;";
         private const string destinationTableName = "EX8_TEMP_PERSON";
-        // private const string _outputRoot = "TestData\\Output\\";
+        // private const string _outputRoot = "TestData\\Output\\";  // Its not being used here.
 
         [Fact]
         public void Can_BulkCopy()
         {
+            var datatocopy = CreateTable(); //first create the datatocopy table which will be copied to destination, we can pass this datatable as well ?
+
             var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
-            var data = CreateTable();
-
             OracleSqlWriter target = new OracleSqlWriter();
-            var outputnoOfRecord = target.BulkCopy(manifestObject.manifest.sourceConnectionString, destinationTableName, manifestObject.manifest.tables[0], data);
+            var outputnoOfRecord = target.BulkCopy(connectionString, destinationTableName, manifestObject.manifest.tables[0], datatocopy);
 
-            Assert.Equal(data.Rows.Count, outputnoOfRecord);
+            Assert.Equal(datatocopy.Rows.Count, outputnoOfRecord);
 
             var reader = new OracleSqlReader();
-            var result = reader.GetData(manifestObject.manifest.sourceConnectionString, $"select * from {destinationTableName}");
+            var result = reader.GetData(connectionString, $"select * from {destinationTableName}");
             Assert.NotEmpty(result.Tables[0].Rows);
         }
 
 
-        //[Fact]
-        //public void Can_BulkUpdate()
-        //{
-        //    var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
-        //    var queryFiles = GetJsonFile<TargetSql>(_inputRoot, "xepdb1.target.person.json");
-        //    var data = CreateTable();
+        [Fact]
+        public void Can_ExecuteUpdateQuery()
+        {
+            var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
+            var queryFiles = GetJsonFile<TargetSql>(_inputRoot, "xepdb1.target.person.json");
+            
+            var datatocopy = CreateTable(); // first get the data to be updated
+            OracleSqlWriter target = new OracleSqlWriter();
+            target.BulkCopy(manifestObject.manifest.sourceConnectionString, destinationTableName, manifestObject.manifest.tables[0], datatocopy); // write data into temp table using this library
 
-        //    OracleSqlWriter target = new OracleSqlWriter();
-        //    var outputnoOfRecord = target.BulkCopy(manifestObject.manifest.sourceConnectionString, destinationTableName, manifestObject.manifest.tables[0], data);
-        //    int outputUpdateRecordCount = target.UpdateBulkData(manifestObject.manifest.sourceConnectionString,queryFiles.UpdateFromTempDml);
-        //}
+            int affectedrows = target.executeUpdateQuery(manifestObject.manifest.sourceConnectionString, queryFiles.UpdateFromTempDml); // update the table from temp
+            Assert.NotEqual(-1, affectedrows); // assuming that if we get number of rows affected = -1 then there is some problem else update worked
+         }
 
         private T GetJsonFile<T>(string root, string file)
         {
@@ -63,11 +65,24 @@ namespace Ex8.SqlDml.Test.Writer
             table.Columns.Add("FIRST_NAME", typeof(string));
             table.Columns.Add("LAST_NAME", typeof(string));
 
-            table.Rows.Add(1, "Daniel", "Saunders");
-            table.Rows.Add(2, "Sonal", "Rattan");
-            table.Rows.Add(3, "Peter", "Lancos");
-            table.Rows.Add(4, "Suraj", "Nittoor");
+            //table.Rows.Add(1, "Daniel", "Saunders");
+            //table.Rows.Add(2, "Sonal", "Rattan");
+            //table.Rows.Add(3, "Peter", "Lancos");
+            //table.Rows.Add(4, "Suraj", "Nittoor");
 
+            // changed this so that to see if I can have different values back into the same table as a test purpose only!
+            table.Rows.Add(1, "Anuj", "Jain");
+            table.Rows.Add(2, "Neha", "Verma");
+            table.Rows.Add(3, "Khushboo", "Jain");
+            table.Rows.Add(4, "Nupur", "Garg");
+            //table.Rows.Add(5, "Anuj1", "Jain1");
+            //table.Rows.Add(6, "Neha1", "Verma1");
+            //table.Rows.Add(7, "DD1", "PP1");
+            //table.Rows.Add(8, "JJ1", "KK1");
+            //table.Rows.Add(9, "Anuj2", "Jain2");
+            //table.Rows.Add(10, "Neha1", "Verma1");
+            //table.Rows.Add(11, "DD1", "PP1");
+            //table.Rows.Add(12, "JJ1", "KK1");
             return table;
         }
     }
