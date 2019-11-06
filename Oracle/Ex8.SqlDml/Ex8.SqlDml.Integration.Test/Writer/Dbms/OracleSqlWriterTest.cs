a472e0b9e9f08c5484cd9c205a6a3518037bdf45
Neha ateam <nvnehaverma40@gmail.com>
2019-11-06 17:55:16 +0530

    Three New methods 1. Can_DBConnect_usingEZConnect1() - connectionString using EasyConnect, test case passing 2. Can_DBConnect_usingEZConnect2 - not passing test case. not able to connect using this way 3. Can_DBConnect_TNSMethod - passing this first creates adds a datasource alias in OracleConfiguration and then we just use that alias into our connectionString. then it connects to database

--- Oracle/Ex8.SqlDml/Ex8.SqlDml.Integration.Test/Writer/Dbms/OracleSqlWriterTest.cs
+++ Oracle/Ex8.SqlDml/Ex8.SqlDml.Integration.Test/Writer/Dbms/OracleSqlWriterTest.cs
@@ -5,6 +5,7 @@ using Ex8.Helper.Serialization;
 using Ex8.SqlDml.Reader.Dbms;
 using Ex8.SqlDml.Writer.Dbms;
 using FluentAssertions;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -51,6 +52,55 @@ namespace Ex8.SqlDml.Integration.Test.Writer.Dbms
             result.Tables[0].Rows.Should().NotBeEmpty();
         }
 
+
+        [Fact]
+        public void Can_DBConnect_usingEZConnect1()
+        {
+
+            string userCreds = @"User Id=TEST_USER; password=ExateDbUser123!;";
+            string dbSource = @"Data Source=oracle1.sql.exatebot.com:1521/xepdb1;";
+            string connectString = userCreds + dbSource;
+            
+            var data = CreateTable();
+            var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
+
+            OracleSqlWriter target = new OracleSqlWriter();
+            var outputnoOfRecord = target.BulkCopy(connectString, manifestObject.manifest.tables[0], data);
+            target.ExecuteSqlText(connectString, new List<string> { });
+            data.Rows.Count.Should().Be(outputnoOfRecord);
+        }
+
+        [Fact]
+        public void Can_DBConnect_usingEZConnect2() 
+        {
+            string connectString = @"Data Source=TEST_USER/ExateDbUser123!@//oracle1.sql.exatebot.com:1521/xepdb1";
+            var data = CreateTable();
+            var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
+            OracleSqlWriter target = new OracleSqlWriter();
+            var outputnoOfRecord = target.BulkCopy(connectString, manifestObject.manifest.tables[0], data);
+            target.ExecuteSqlText(connectString, new List<string> { });
+            data.Rows.Count.Should().Be(outputnoOfRecord);
+        }
+
+        [Fact]
+        public void Can_DBConnect_TNSMethod()
+        {
+            _OracleDataSourceConnectionConfig("orclpdb");
+            string connectString = "user id=TEST_USER; password=ExateDbUser123!; data source=orclpdb;";
+
+            var data = CreateTable();
+            var manifestObject = GetJsonFile<DatabaseJobManifest>(_inputRoot, "database.manifest.xepdb1.json");
+            OracleSqlWriter target = new OracleSqlWriter();
+            var outputnoOfRecord = target.BulkCopy(connectString, manifestObject.manifest.tables[0], data);
+            target.ExecuteSqlText(connectString, new List<string> { });
+            data.Rows.Count.Should().Be(outputnoOfRecord);
+        }
+
+
+
+
+
+
         private T GetJsonFile<T>(string root, string file)
         {
             var outputPath = Path.Combine(AssemblyHelper.GetCurrentExecutingAssemblyPath(), string.Format("{0}{1}", root, file));
@@ -77,5 +127,12 @@ namespace Ex8.SqlDml.Integration.Test.Writer.Dbms
             //table.Rows.Add(4, "Nupur", "Garg");
             return table;
         }
+
+
+        internal void _OracleDataSourceConnectionConfig(string dsname)
+        {
+            OracleConfiguration.OracleDataSources.Add(dsname, "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle1.sql.exatebot.com)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=xepdb1)(SERVER=dedicated)))");
+        }
+
     }
 }
