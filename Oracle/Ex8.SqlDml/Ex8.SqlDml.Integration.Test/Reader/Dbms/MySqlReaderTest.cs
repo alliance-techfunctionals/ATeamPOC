a32adca91f76dfac4d4b4560e88a6db5100c0cfc
Neha ateam <nvnehaverma40@gmail.com>
2019-11-13 18:06:54 +0530

    changed the connection string since this is our godaddy database connection string

--- Oracle/Ex8.SqlDml/Ex8.SqlDml.Integration.Test/Reader/Dbms/MySqlReaderTest.cs
+++ Oracle/Ex8.SqlDml/Ex8.SqlDml.Integration.Test/Reader/Dbms/MySqlReaderTest.cs
@@ -9,15 +9,15 @@ namespace Ex8.SqlDml.Integration.Test
 {
     public class MySqlReaderTest
     {
-        private const string connectionString = "server=localhost;port=3306;user='root';password='';database=ex8db1";
+        private const string connectionString = "server=182.50.133.84;port=3306;user='ex8db1';password='ex8db1@123';database=ex8_db1";
 
         [Fact]
         public void Can_GetData()
         {
             var target = new MySqlReader();
-            var dataTable = target.GetData(connectionString, $"Select * from ex8db1.ATeam");
+            var dataTable = target.GetData(connectionString, $"Select * from ex8_db1.ATeam");
 
-            int expectedOutput = target.ExecuteScalar<int>(connectionString, $"Select count(*) from ex8db1.ATeam");
+            int expectedOutput = target.ExecuteScalar<int>(connectionString, $"Select count(*) from ex8_db1.ATeam");
             int actualOutput = dataTable.Tables[0].Rows.Count;
 
             dataTable.Tables.Should().NotBeEmpty();
