6a4c80f11f13955843a488520151e1de1abb58fe
Anuj <anuj@alliance-techfunctionals.com>
2019-11-16 17:51:07 +0530

    Created a Generic class to Convert DataTable to IEnumerable List

---
+++ Oracle/Ex8.Helper/Ex8.Helper/Converter/DataTableHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Linq;
+
+namespace Ex8.Helper.Converter
+{
+
+    public static class DataTableHelper
+    {
+        public static List<T> DataTableToList<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
