8ef013ad51f919d4cea57808a0613c01f090df26
Anuj <anuj@alliance-techfunctionals.com>
2019-11-16 17:49:30 +0530

    Created a Model class to Map the Properties in PostgreSQLCopyHelper.

---
+++ Oracle/Ex8.EtlModel/Ex8.EtlModel/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex8.EtlModel
+{
+    public class Customer
+    {
+        public Int32 Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public Int32 Age { get; set; }
+    }
+}
