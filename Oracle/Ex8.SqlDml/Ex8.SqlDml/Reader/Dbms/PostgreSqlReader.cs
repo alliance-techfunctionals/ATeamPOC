a6302ea00fceac8799ce1d372e29ea39be9dd15f
Anuj <anuj@alliance-techfunctionals.com>
2019-11-16 17:53:08 +0530

    PostgreSql Reader class

---
+++ Oracle/Ex8.SqlDml/Ex8.SqlDml/Reader/Dbms/PostgreSqlReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using Ex8.EtlModel.DatabaseJobManifest;
+using Npgsql;
+using System.Linq;
+
+namespace Ex8.SqlDml.Reader.Dbms
+{
+    public class PostgreSqlReader : ISqlReader
+    {
+        public DatabaseTypeEnum DatabaseType => DatabaseTypeEnum.Postgres;
+
+        public T ExecuteScalar<T>(string connectionString, string sql)
+        {
+            using (var targetConn = new NpgsqlConnection(connectionString))
+            {
+                return targetConn.ExecuteScalar<T>(sql);
+            }
+        }
+
+        public IEnumerable<T> ExecuteQuery<T>(string connectionString, string sql)
+        {
+            using (var targetConn = new NpgsqlConnection(connectionString))
+            {
+                return targetConn.Query<T>(sql);
+            }
+        }
+
+        public DataSet GetData(string connectionString, string selectSql)
+        {
+            var ds = new DataSet();
+            using (var connection = new NpgsqlConnection(connectionString))
+            using (var adapter = new NpgsqlDataAdapter(selectSql, connectionString))
+            {
+                adapter.Fill(ds);
+                return ds;
+            }
+        }
+    }
+}
